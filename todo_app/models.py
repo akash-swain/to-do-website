7fdb832817da06ef4d036aedd357797415963081
= <=>
2019-05-09 12:48:26 +0530

    m

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
     content = models.CharField(max_length = 100)
+    owner = models.ForeignKey(User,on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.content
+        # return (f"{owner}-{self.content}")
+        return (self.owner, self.content)
